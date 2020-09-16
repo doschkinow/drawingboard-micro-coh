7250f1ad2512380a2b62634310a396fe6c02398b
pdoschki <pdoschki@pirin.de.oracle.com>
2020-09-16 14:23:44 +0200

    minor fixes

--- k8s/startup.bash
+++ k8s/startup.bash
@@ -3,13 +3,13 @@
 #
 
 jarfile=$(ls /pipeline/source/target/*with-dependencies.jar)
-options="-Dcoherence.cacheconfig=drawingboard-coherence-cache-config.xml"
-wka_options="-Dcoherence.wka=cache"
+options="-Dcoherence.cacheconfig=drawingboard-coherence-cache-config.xml -Dcoherence.cluster=drawingboard"
+wka_options="-Dcoherence.wka=drawingboard-cache"
 
 echo wait a little so cache service gets its ip
 sleep 5
 
-ping -c 1 -W 1 cache
+ping -c 1 -W 1 drawingboard-cache
 if [ $? -ne 0 ]
 then
         echo "exiting since no cache service available..."
